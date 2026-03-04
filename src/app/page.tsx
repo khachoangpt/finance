7dba51687040e041e995a1f94882e33fa1ff8115
Hoang Nguyen <hoangncd3@gmail.com>
2026-03-04 09:18:35 +0700

    fix: no store response

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
 import { HomeTemplate } from "@/modules/home";
 
 export default async function Home() {
-  const goldPrices = await fetch("https://www.vang.today/api/prices.php").then(
-    (res) => res.json(),
-  );
+  const goldPrices = await fetch("https://www.vang.today/api/prices.php", {
+    cache: "no-store",
+  }).then((res) => res.json());
   const pricesRange = await fetch(
     `https://www.vang.today/api/prices.php?days=30`,
+    {
+      cache: "no-store",
+    },
   ).then((res) => res.json());
 
   return (
--- src/components/base/data-table/data-table.tsx
+++ src/components/base/data-table/data-table.tsx
@@ -31,7 +31,7 @@ export function DataTable<TData, TValue>({
   });
 
   return (
-    <div className="overflow-hidden rounded-md border">
+    <div className="overflow-auto rounded-md border">
       <Table>
         <TableHeader>
           {table.getHeaderGroups().map((headerGroup) => (
